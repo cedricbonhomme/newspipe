91feb619752473b2be958e58bf34e45307c2dc19
cedricbonhomme <devnull@localhost>
2010-05-04 08:09:13 +0200

    Release 1.3. Export function (HTML, raw text).

--- pyAggr3g470r.py
+++ pyAggr3g470r.py
@@ -2,8 +2,8 @@
 #-*- coding: utf-8 -*-
 
 __author__ = "Cedric Bonhomme"
-__version__ = "$Revision: 1.2 $"
-__date__ = "$Date: 2010/04/29 $"
+__version__ = "$Revision: 1.3 $"
+__date__ = "$Date: 2010/05/04 $"
 __copyright__ = "Copyright (c) 2010 Cedric Bonhomme"
 __license__ = "GPLv3"
 
