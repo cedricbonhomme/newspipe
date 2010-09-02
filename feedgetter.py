818492abf704df92aefe8444e0d0b237f232806b
cedricbonhomme <devnull@localhost>
2010-09-02 11:14:17 +0200

    Release 1.9. Improvement of the feedgetter module. More details about articles are sotred in the data base (when it is possible).

--- feedgetter.py
+++ feedgetter.py
@@ -2,8 +2,8 @@
 #-*- coding: utf-8 -*-
 
 __author__ = "Cedric Bonhomme"
-__version__ = "$Revision: 0.9 $"
-__date__ = "$Date: 2010/04/15 $"
+__version__ = "$Revision: 1.0 $"
+__date__ = "$Date: 2010/09/02 $"
 __copyright__ = "Copyright (c) 2010 Cedric Bonhomme"
 __license__ = "GPLv3"
 
--- pyAggr3g470r.py
+++ pyAggr3g470r.py
@@ -2,8 +2,8 @@
 #-*- coding: utf-8 -*-
 
 __author__ = "Cedric Bonhomme"
-__version__ = "$Revision: 1.8 $"
-__date__ = "$Date: 2010/08/25 $"
+__version__ = "$Revision: 1.9 $"
+__date__ = "$Date: 2010/09/02 $"
 __copyright__ = "Copyright (c) 2010 Cedric Bonhomme"
 __license__ = "GPLv3"
 
