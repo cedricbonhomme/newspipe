176adb7f345c3f26f6c3d03dbb1f4e8e5c8a89a1
cedricbonhomme <devnull@localhost>
2012-10-12 08:32:19 +0200

    New version number.

--- source/auth.py
+++ source/auth.py
@@ -1,5 +1,31 @@
 #! /usr/bin/env python
 #-*- coding: utf-8 -*-
+
+# pyAggr3g470r - A Web based news aggregator.
+# Copyright (C) 2010-2012  Cédric Bonhomme - http://cedricbonhomme.org/
+#
+# For more information : http://bitbucket.org/cedricbonhomme/pyaggr3g470r/
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+__author__ = "Cedric Bonhomme"
+__version__ = "$Revision: 0.1 $"
+__date__ = "$Date: 2012/10/12 $"
+__revision__ = "$Date: 2012/10/12 $"
+__copyright__ = "Copyright (c) Cedric Bonhomme"
+__license__ = "GPLv3"
+
 #
 # Form based authentication for CherryPy. Requires the
 # Session tool to be loaded.
--- source/pyAggr3g470r.py
+++ source/pyAggr3g470r.py
@@ -20,7 +20,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 __author__ = "Cedric Bonhomme"
-__version__ = "$Revision: 3.4 $"
+__version__ = "$Revision: 3.5 $"
 __date__ = "$Date: 2010/01/29 $"
 __revision__ = "$Date: 2012/05/01 $"
 __copyright__ = "Copyright (c) Cedric Bonhomme"
