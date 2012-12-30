ea3f5fc2833ec760bf3c9e1307e999a9a3042408
Cédric Bonhomme <kimble.mandel@gmail.com>
2012-12-30 21:53:09 +0100

    Bugfix in export.py for export_txt().

--- source/export.py
+++ source/export.py
@@ -112,14 +112,14 @@ def export_txt(mongo_db):
     for feed in feeds:
         # creates folder for each stream
         folder = conf.path + "/var/export/txt/" + \
-                utils.normalize_filename(feed["feed_title"].strip().replace(':', '').lower().encode('utf-8'))
+                utils.normalize_filename(feed["feed_title"].strip().replace(':', '').lower())
         try:
             os.makedirs(folder)
         except OSError:
             # directories already exists (not a problem)
             pass
 
-        for article in mongo_db.get_articles_from_collection(feed["feed_id"]):
+        for article in mongo_db.get_articles(feed_id=feed["feed_id"]):
             name = article["article_date"].ctime().strip().replace(' ', '_')
             name = os.path.normpath(folder + "/" + name + ".txt")
 
@@ -127,7 +127,7 @@ def export_txt(mongo_db):
             content += utils.clear_string(article["article_content"])
 
             with open(name, "w") as f:
-                f.write(content.encode('utf-8'))
+                f.write(content)
 
 def export_epub(mongo_db):
     """
