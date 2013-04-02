7da89e4e079567b480a459cef5b77509626dd598
Cédric Bonhomme <kimble.mandel@gmail.com>
2013-04-02 10:07:57 +0200

    Updated revision date.

--- source/mongodb.py
+++ source/mongodb.py
@@ -20,9 +20,9 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 __author__ = "Cedric Bonhomme"
-__version__ = "$Revision: 0.6 $"
+__version__ = "$Revision: 0.7 $"
 __date__ = "$Date: 2012/03/03 $"
-__revision__ = "$Date: 2013/02/01 $"
+__revision__ = "$Date: 2013/04/02 $"
 __copyright__ = "Copyright (c) Cedric Bonhomme"
 __license__ = "GPLv3"
 
