6f5f4f413eee916eac13294de6ea58d2d6a559e1
Cédric Bonhomme <kimble.mandel@gmail.com>
2013-06-10 07:31:01 +0200

    Changed header information in feedgetter.py.

--- source/feedgetter.py
+++ source/feedgetter.py
@@ -20,9 +20,9 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 __author__ = "Cedric Bonhomme"
-__version__ = "$Revision: 1.5 $"
+__version__ = "$Revision: 1.6 $"
 __date__ = "$Date: 2010/09/02 $"
-__revision__ = "$Date: 2013/04/02 $"
+__revision__ = "$Date: 2013/06/10 $"
 __copyright__ = "Copyright (c) Cedric Bonhomme"
 __license__ = "GPLv3"
 
