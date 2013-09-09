90a893d43f9e8b05045dc2cc5071841006bc4fac
Cédric Bonhomme <kimble.mandel@gmail.com>
2013-09-09 07:11:56 +0200

    Improved pymongo request

--- source/pyAggr3g470r.py
+++ source/pyAggr3g470r.py
@@ -540,8 +540,8 @@ class pyAggr3g470r(object):
         sha1_hash = hashlib.sha1()
         sha1_hash.update(new_feed_url.encode('utf-8'))
         new_feed_id = sha1_hash.hexdigest()
-        self.mongo.update_feed(feed_id, {"feed_link":new_feed_url})
-        self.mongo.update_feed(feed_id, {"feed_id":new_feed_id})
+        self.mongo.update_feed(feed_id, {"feed_id":new_feed_id,
+                                        "feed_link":new_feed_url})
         result = utils.change_feed_url(old_feed_url, new_feed_url)
         if result:
             tmpl = lookup.get_template("confirmation.html")
