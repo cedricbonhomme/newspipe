6a8abd977fa22411a5fe5c6c3e1336e406566cea
Cédric Bonhomme <kimble.mandel@gmail.com>
2013-10-30 19:08:45 +0100

    this sort is enough fast

--- pyaggr3g470r/feedgetter.py
+++ pyaggr3g470r/feedgetter.py
@@ -136,7 +136,7 @@ class FeedGetter(object):
                                                             article_title, description)).start()
             """
         feed.articles.extend(articles)
-        #feed.articles = sorted(feed.articles, key=lambda t: t.date, reverse=True)
+        feed.articles = sorted(feed.articles, key=lambda t: t.date, reverse=True)
         feed.save()
 
 
