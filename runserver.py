6365f149ae3e1daa0f9b0ffd223180935d8bcc88
Cédric Bonhomme <cedric@cedricbonhomme.org>
2014-01-12 18:59:35 +0100

    Uses the host address and port number specified in the configuration file.

--- runserver.py
+++ runserver.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import conf
 from pyaggr3g470r import app
 
-app.run(host='0.0.0.0', debug=True)
+app.run(host=conf.WEBSERVER_HOST, port=conf.WEBSERVER_PORT, \
+        debug=conf.WEBSERVER_DEBUG)
