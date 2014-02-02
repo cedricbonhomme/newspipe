4a37f515c0982bdd1b11cb83482e9a35fb67a2a2
Cédric Bonhomme <cedric@cedricbonhomme.org>
2014-02-02 18:08:57 +0100

    Affed fetch.py script to use with cron.

---
+++ fetch.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# fetch.py
+#
+# You can use this script with cron, for example:
+#  */30 * * * * cd ~/.pyaggr3g470r/ ; python fetch.py
+# to fetch articles every 30 minutes.
+
+import sys
+import conf
+from pyaggr3g470r import feedgetter
+
+if __name__ == "__main__":
+    # Point of entry in execution mode
+    print sys.argv[1]
+    feed_getter = feedgetter.FeedGetter(sys.argv[1])
+    feed_getter.retrieve_feed(None)
