86df3f45a13733b1a73ad33975d24fff7fe82e7f
Cédric Bonhomme <kimble.mandel@gmail.com>
2014-04-09 08:18:03 +0200

    License is now AGPLv3.

--- runserver.py
+++ runserver.py
@@ -1,6 +1,24 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# pyAggr3g470r - A Web based news aggregator.
+# Copyright (C) 2010-2014  Cédric Bonhomme - http://cedricbonhomme.org/
+#
+# For more information : https://bitbucket.org/cedricbonhomme/pyaggr3g470r/
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import conf
 from pyaggr3g470r import app
 
