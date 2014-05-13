f47c5091c4534e9e7301518b86b8f56b658aaf94
Cédric Bonhomme <cedric@cedricbonhomme.org>
2014-05-13 22:14:40 +0200

    Auto confirm admin account.

--- db_create.py
+++ db_create.py
@@ -69,7 +69,8 @@ role_user = Role(name="user")
 
 user1 = User(nickname="admin",
             email="root@pyAggr3g470r.localhost",
-            pwdhash=generate_password_hash("password"))
+            pwdhash=generate_password_hash("password"),
+            activation_key="")
 user1.roles.extend([role_admin, role_user])
 
 db.session.add(user1)
