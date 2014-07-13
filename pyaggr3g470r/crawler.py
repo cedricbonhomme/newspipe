87c476402aaa8c04f8a69baa10702ecfff720a77
Cédric Bonhomme <cedric@cedricbonhomme.org>
2014-07-13 15:36:04 +0200

    Update headers tags.

--- pyaggr3g470r/crawler.py
+++ pyaggr3g470r/crawler.py
@@ -20,9 +20,9 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 __author__ = "Cedric Bonhomme"
-__version__ = "$Revision: 2.1 $"
+__version__ = "$Revision: 2.2 $"
 __date__ = "$Date: 2010/09/02 $"
-__revision__ = "$Date: 2014/04/13 $"
+__revision__ = "$Date: 2014/07/13 $"
 __copyright__ = "Copyright (c) Cedric Bonhomme"
 __license__ = "AGPLv3"
 
