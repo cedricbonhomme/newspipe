46910ee27d19f18758d188d2991c8fc51072aba5
Cédric Bonhomme <cedric@cedricbonhomme.org>
2014-11-21 09:01:33 +0100

    Test with Vagrant.

---
+++ vagrant/bootstrap.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+apt-get update
+apt-get upgrade
+apt-get install -y python python-pip git
+
+# Clone the source code of pyAggr3g470r
+git clone https://bitbucket.org/cedricbonhomme/pyaggr3g470r.git
+if [ $? -ne 0 ]; then
+    echo "\nERROR: unable to clone the git repository\n"
+    exit 1;
+fi
+
+# Install all requierements
+cd pyaggr3g470r
+apt-get install -y libxml2-dev libxslt1-dev
+pip install --upgrade -r requirements.txt
+cp conf/conf.cfg-sample conf/conf.cfg
+cd ..
+
+# Installation of PostgreSQL
+apt-get install -y postgresql postgresql-server-dev-9.3 postgresql-client
+
+# Configuration of the database
+echo "127.0.0.1:5432:aggregator:vagrant:xxYzToW42" > .pgpass
+chmod 700 .pgpass
+sudo -u postgres createuser vagrant --no-superuser --createdb --no-createrole
+createdb aggregator --no-password
+echo "ALTER USER vagrant WITH ENCRYPTED PASSWORD 'xxYzToW42';" | sudo -u postgres psql
+echo "GRANT ALL PRIVILEGES ON DATABASE aggregator TO vagrant;" | sudo -u postgres psql
+
+# Initializes the database
+cd pyaggr3g470r
+python db_create.py
+
+# start pyAggr3g470r at startup
+echo "#!/bin/sh -e" > /etc/rc.local
+echo "su vagrant python runserver.py" >> /etc/rc.local
+echo "exit 0" >> /etc/rc.local
+chmod 755 /etc/rc.local
+
+# Start the application
+chown -R vagrant:vagrant ..
+/etc/init.d/rc.local start
