42fc65f5cb107496a0ae64ea1cc872a33d233f97
Cédric Bonhomme <cedric@cedricbonhomme.org>
2015-02-09 08:55:49 +0100

    Updated conf file for Vagrant.

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -2,7 +2,7 @@
 
 apt-get update
 apt-get upgrade
-apt-get install -y python libpq-dev python-dev python-pip build-essential git
+apt-get install -y python3 libpq-dev python3-dev build-essential git
 
 # Clone the source code of pyAggr3g470r
 git clone https://bitbucket.org/cedricbonhomme/pyaggr3g470r.git
@@ -16,7 +16,7 @@ cd pyaggr3g470r
 # For lxml:
 apt-get install -y libxml2-dev libxslt1-dev
 # installation with pip
-sudo pip install --upgrade -r requirements.txt
+sudo pip3 install --upgrade -r requirements.txt
 # copy of the default configuration files for vagrant
 cp vagrant/conf.cfg-sample conf/conf.cfg
 cd ..
@@ -35,12 +35,12 @@ echo "GRANT ALL PRIVILEGES ON DATABASE aggregator TO vagrant;" | sudo -u postgre
 # Initializes the database
 cd pyaggr3g470r
 chown -R vagrant:vagrant .
-sudo -u vagrant python db_create.py
+sudo -u vagrant python3 db_create.py
 
 # start pyAggr3g470r at startup
 echo "#!/bin/sh -e" > /etc/rc.local
 echo "cd /home/vagrant/pyaggr3g470r/" >> /etc/rc.local
-echo "sudo -u vagrant python runserver.py &" >> /etc/rc.local
+echo "sudo -u vagrant python3 runserver.py &" >> /etc/rc.local
 echo "exit 0" >> /etc/rc.local
 chmod 755 /etc/rc.local
 
