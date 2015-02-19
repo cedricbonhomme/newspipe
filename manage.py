20281e5e2ee3f7d7282620bffa208686c42575b1
Cédric Bonhomme <cedric@cedricbonhomme.org>
2015-02-19 07:39:00 +0100

    Enable a remote upgrade on Heroku with Flask-migrate.

---
+++ manage.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
+
+from pyaggr3g470r import app, db
+#app.config.from_object(os.environ['APP_SETTINGS'])
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
