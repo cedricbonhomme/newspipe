2d05b4853ef8cf77389d99b7b6cd34f13986aa1e
Cédric Bonhomme <cedric@cedricbonhomme.org>
2015-03-01 19:07:31 +0100

    This is now useless.

--- bootstrap.py
+++ bootstrap.py
@@ -1,17 +1,7 @@
 # required imports and code exection for basic functionning
 
-import sys
-if 'threading' in sys.modules:
-    raise Exception('threading module loaded before patching!')
 import conf
 import logging
-
-if not (conf.WEBSERVER_DEBUG or conf.ON_HEROKU):
-    import gevent.monkey
-    gevent.monkey.patch_thread()
-
-
-
 
 def set_logging(log_path, log_level=logging.INFO,
                 log_format='%(asctime)s %(levelname)s %(message)s'):
