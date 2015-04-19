4dc45b55c737bab61a8081d3d93c30489218d0c5
Cédric Bonhomme <cedric@cedricbonhomme.org>
2015-04-19 10:24:46 +0200

    Remove the HTML 500 error handler to better understand the problem.

--- pyaggr3g470r/views/views.py
+++ pyaggr3g470r/views/views.py
@@ -114,9 +114,9 @@ def authentication_failed(e):
 def page_not_found(e):
     return render_template('errors/404.html'), 404
 
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('errors/500.html'), 500
+#@app.errorhandler(500)
+#def internal_server_error(e):
+    #return render_template('errors/500.html'), 500
 
 
 def redirect_url(default='home'):
