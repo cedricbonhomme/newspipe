9e1865b93da2c5eb3bad1295cb1ce542447a75fa
Cédric Bonhomme <cedric@cedricbonhomme.org>
2015-05-27 10:40:17 +0200

    Feedpaser is now fixed.

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -20,11 +20,6 @@ cd pyaggr3g470r
 apt-get install -y libxml2-dev libxslt1-dev
 # installation with pip
 sudo pip3 install --upgrade -r requirements.txt
-wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py
-sudo python3 get-pip.py
-rm get-pip.py
-sudo pip3 uninstall -y feedparser
-sudo pip3 install feedparser==5.1.2
 # copy of the default configuration files for vagrant
 cp vagrant/conf.cfg-sample conf/conf.cfg
 cd ..
