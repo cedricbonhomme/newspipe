791db98c55373abfb8a5b4985ad004370f809b97
Cédric Bonhomme <cedric@cedricbonhomme.org>
2016-02-13 11:07:22 +0100

    Again problems with alembic...

--- migrations/versions/7f973d010686_add_updated_date_column_to_article_table.py
+++ migrations/versions/7f973d010686_add_updated_date_column_to_article_table.py
@@ -9,8 +9,6 @@ Create Date: 2016-02-12 21:51:40.868539
 # revision identifiers, used by Alembic.
 revision = '7f973d010686'
 down_revision = '25ca960a207'
-branch_labels = None
-depends_on = None
 
 from alembic import op
 import sqlalchemy as sa
