824b5044eb22a433d966dd94f87a3e62c540f66f
Cédric Bonhomme <cedric@cedricbonhomme.org>
2016-02-18 09:04:13 +0100

    app -> application

--- src/web/lib/user_utils.py
+++ src/web/lib/user_utils.py
@@ -6,16 +6,16 @@ from bootstrap import application
 
 
 def generate_confirmation_token(email):
-    serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
-    return serializer.dumps(email, salt=app.config['SECURITY_PASSWORD_SALT'])
+    serializer = URLSafeTimedSerializer(application.config['SECRET_KEY'])
+    return serializer.dumps(email, salt=application.config['SECURITY_PASSWORD_SALT'])
 
 
 def confirm_token(token, expiration=3600):
-    serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    serializer = URLSafeTimedSerializer(application.config['SECRET_KEY'])
     try:
         email = serializer.loads(
             token,
-            salt=app.config['SECURITY_PASSWORD_SALT'],
+            salt=application.config['SECURITY_PASSWORD_SALT'],
             max_age=expiration
         )
     except:
