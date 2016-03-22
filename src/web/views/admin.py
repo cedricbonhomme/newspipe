966ce619b60d69423f15b9c80dc983e85eaf34a5
Cédric Bonhomme <cedric@cedricbonhomme.org>
2016-03-22 15:01:39 +0100

    create a new user is now working

--- src/web/views/admin.py
+++ src/web/views/admin.py
@@ -2,10 +2,12 @@ from datetime import datetime
 from flask import (Blueprint, render_template, redirect, flash, url_for)
 from flask.ext.babel import gettext, format_timedelta
 from flask.ext.login import login_required, current_user
+from werkzeug import generate_password_hash
 
 from web.views.common import admin_permission
 from web.lib.utils import redirect_url
 from web.controllers import UserController, ArticleController
+from web.forms import InformationMessageForm, UserForm
 
 admin_bp = Blueprint('admin', __name__, url_prefix='/admin')
 
@@ -16,10 +18,12 @@ admin_bp = Blueprint('admin', __name__, url_prefix='/admin')
 def dashboard():
     last_cons, now = {}, datetime.utcnow()
     users = list(UserController().read().order_by('id'))
+    form = InformationMessageForm()
     for user in users:
-        last_cons[user.id] = format_timedelta(now - user.last_connection)
+        last_cons[user.id] = format_timedelta(now - user.last_seen)
     return render_template('admin/dashboard.html', now=datetime.utcnow(),
-            last_cons=last_cons, users=users, current_user=current_user)
+            last_cons=last_cons, users=users, current_user=current_user,
+            form=form)
 
 
 @admin_bp.route('/user/<int:user_id>', methods=['GET'])
@@ -39,6 +43,59 @@ def user(user_id=None):
     else:
         flash(gettext('This user does not exist.'), 'danger')
         return redirect(redirect_url())
+
+
+@admin_bp.route('/user/create', methods=['GET'])
+@admin_bp.route('/user/edit/<int:user_id>', methods=['GET'])
+@login_required
+@admin_permission.require(http_exception=403)
+def user_form(user_id=None):
+    if user_id is not None:
+        user = UserController().get(id=user_id)
+        form = UserForm(obj=user)
+        message = gettext('Edit the user <i>%(nick)s</i>', nick=user.nickname)
+    else:
+        form = UserForm()
+        message = gettext('Add a new user')
+    return render_template('/admin/create_user.html',
+                           form=form, message=message)
+
+
+@admin_bp.route('/user/create', methods=['POST'])
+@admin_bp.route('/user/edit/<int:user_id>', methods=['POST'])
+@login_required
+@admin_permission.require(http_exception=403)
+def process_user_form(user_id=None):
+    """
+    Create or edit a user.
+    """
+    form = UserForm()
+    user_contr = UserController()
+
+    if not form.validate():
+        return render_template('/admin/create_user.html', form=form,
+                               message=gettext('Some errors were found'))
+
+    if user_id is not None:
+        # Edit a user
+        user_contr.update({'id': user_id},
+                          {'nickname': form.nickname.data,
+                           'email': form.email.data,
+                           'password': form.password.data,
+                           'refresh_rate': form.refresh_rate.data})
+        user = user_contr.get(id=user_id)
+        flash(gettext('User %(nick)s successfully updated',
+                      nick=user.nickname), 'success')
+    else:
+        # Create a new user (by the admin)
+        user = user_contr.create(nickname=form.nickname.data,
+                                 email=form.email.data,
+                                 pwdhash=generate_password_hash(form.password.data),
+                                 is_admin=False,
+                                 refresh_rate=form.refresh_rate.data)
+        flash(gettext('User %(nick)s successfully created',
+                      nick=user.nickname), 'success')
+    return redirect(url_for('admin.user_form', user_id=user.id))
 
 
 @admin_bp.route('/toggle_user/<int:user_id>', methods=['GET'])
