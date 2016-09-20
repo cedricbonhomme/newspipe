2abfc7c43131e0366137b27b0b75132da6ef6582
Cédric Bonhomme <cedric@cedricbonhomme.org>
2016-09-20 15:31:12 +0200

    simple test of a 'popular' page.

--- src/web/views/views.py
+++ src/web/views/views.py
@@ -1,9 +1,11 @@
 import logging
+from collections import Counter
 from flask import (request, render_template, flash,
                    url_for, redirect, current_app)
 from flask_babel import gettext
 
 from conf import API_ROOT
+from web.controllers import FeedController
 from web.lib.view_utils import etag_match
 
 logger = logging.getLogger(__name__)
@@ -38,6 +40,17 @@ def internal_server_error(error):
 @current_app.errorhandler(AssertionError)
 def handle_sqlalchemy_assertion_error(error):
     return error.args[0], 400
+
+
+@current_app.route('/popular', methods=['GET'])
+@etag_match
+def popular():
+    feeds = FeedController().read().all()
+    counter = Counter()
+    for feed in feeds:
+        counter[feed.link] += 1
+    print(counter.most_common(50))
+    return render_template('popular.html', popular=counter.most_common(50))
 
 
 @current_app.route('/about', methods=['GET'])
