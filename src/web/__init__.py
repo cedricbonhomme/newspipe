de05845887181f949780f5627570713b4d3ca93b
Cédric Bonhomme <cedric@cedricbonhomme.org>
2017-04-06 07:49:12 +0200

    Added a page which displays some information about the instance.

---
+++ src/web/__init__.py
@@ -0,0 +1,8 @@
+__author__ = "Cedric Bonhomme"
+__version__ = "$Revision: 7.1.3 $"
+__date__ = "$Date: 2016/11/14 $"
+__revision__ = "$Date: 2016/11/14 $"
+__copyright__ = "Copyright (c) Cedric Bonhomme"
+__license__ = "GPLv3"
+
+__all__ = [__version__]
--- src/web/views/views.py
+++ src/web/views/views.py
@@ -1,3 +1,4 @@
+import sys
 import logging
 import operator
 from datetime import datetime, timedelta
@@ -6,8 +7,9 @@ from flask import (request, render_template, flash,
 from flask_babel import gettext
 from sqlalchemy import desc
 
+from web import __version__
 from conf import API_ROOT, ADMIN_EMAIL
-from web.controllers import FeedController
+from web.controllers import FeedController, UserController
 from web.lib.view_utils import etag_match
 
 logger = logging.getLogger(__name__)
@@ -74,9 +76,15 @@ def popular():
 @current_app.route('/about', methods=['GET'])
 @etag_match
 def about():
-    print(ADMIN_EMAIL)
     return render_template('about.html', contact=ADMIN_EMAIL)
 
+@current_app.route('/about/more', methods=['GET'])
+@etag_match
+def about_more():
+    return render_template('about_more.html',
+                        newspipe_version=__version__.split()[1],
+                        python_version="{}.{}.{}".format(*sys.version_info[:3]),
+                        nb_users=UserController().read().count())
 
 @current_app.route('/.well-known/acme-challenge/MmwFRp_wOgBGHcIULSUGVFDjpryEw_uWz7UgD6rE4t4')
 def letsencrypt():
