7165907b2e0628efc041e55c7162eb6a18ab545f
Cédric Bonhomme <cedric@cedricbonhomme.org>
2017-05-25 00:07:02 +0200

    Bumped version number for Heroku.

--- src/web/__init__.py
+++ src/web/__init__.py
@@ -1,7 +1,7 @@
 __author__ = "Cedric Bonhomme"
-__version__ = "$Revision: 7.1.3 $"
+__version__ = "$Revision: 8.0 $"
 __date__ = "$Date: 2016/11/14 $"
-__revision__ = "$Date: 2016/11/14 $"
+__revision__ = "$Date: 2017/05/24 $"
 __copyright__ = "Copyright (c) Cedric Bonhomme"
 __license__ = "GPLv3"
 
