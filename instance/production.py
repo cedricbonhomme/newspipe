81040bb1af17d279d4690b186f94b93635148a86
Cédric Bonhomme <cedric@cedricbonhomme.org>
2020-03-10 11:36:59 +0100

    Fixed typo.

--- instance/production.py
+++ instance/production.py
@@ -1,11 +1,15 @@
+import os
+
 # Webserver
 HOST = "127.0.0.1"
 PORT = 5000
 DEBUG = False
 API_ROOT = "/api/v2.0"
 
+CSRF_ENABLED = True
 SECRET_KEY = "LCx3BchmHRxFzkEv4BqQJyeXRLXenf"
 SECURITY_PASSWORD_SALT = "L8gTsyrpRQEF8jNWQPyvRfv7U5kJkD"
+TOKEN_VALIDITY_PERIOD = 3600
 
 # Database
 DB_CONFIG_DICT = {
@@ -24,10 +28,10 @@ CRAWLING_METHOD = "default"
 DEFAULT_MAX_ERROR = 3
 HTTP_PROXY = ""
 CRAWLER_USER_AGENT = "Newspipe (https://git.sr.ht/~cedric/newspipe)"
-CRAWLER_TIMEOUT = 5
+CRAWLER_TIMEOUT = 30
 CRAWLER_RESOLV = False
 RESOLVE_ARTICLE_URL = False
-FEED_REFRESH_INTERVAL = 100
+FEED_REFRESH_INTERVAL = 120
 
 # Notification
 MAIL_SERVER = "localhost"
@@ -40,8 +44,10 @@ MAIL_PASSWORD = None
 MAIL_DEFAULT_SENDER = ADMIN_EMAIL
 
 # Misc
+BASE_DIR = os.path.abspath(os.path.dirname('.'))
+LANGUAGES = {"en": "English", "fr": "French"}
+TIME_ZONE = {"en": "US/Eastern", "fr": "Europe/Paris"}
 ADMIN_EMAIL = "admin@admin.localhost"
-TOKEN_VALIDITY_PERIOD = 3600
 LOG_LEVEL = "info"
 LOG_PATH = "./var/newspipe.log"
 SELF_REGISTRATION = True
