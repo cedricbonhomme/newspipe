22c0d55395e631c8b53f80584ae0a4b559955db8
Cédric Bonhomme <cedric@cedricbonhomme.org>
2020-03-20 23:01:29 +0100

    redirect output of db_create to /dev/null

--- wait-for-postgres.sh
+++ wait-for-postgres.sh
@@ -14,6 +14,6 @@ done
 
 >&2 echo "Postgres is up - executing command"
 export Newspipe_CONFIG=/newspipe/instance/production.py
-poetry run ./manager.py db_create
+poetry run ./manager.py db_create >/dev/null
 poetry run pybabel compile -d newspipe/translations
 poetry run ./runserver.py
