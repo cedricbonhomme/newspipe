b8c23a00b8b1d4ce93c52253a758301e07e4534e
Cédric Bonhomme <cedric@cedricbonhomme.org>
2020-06-27 00:54:26 +0200

    chg: [template] Minor improvements and fixed to the edit_feed.html template.

--- newspipe/static/js/feed.js
+++ newspipe/static/js/feed.js
@@ -1,29 +1,39 @@
 var node = document.getElementById('add-feed-filter-row');
 if (node != null) {
   node.onclick = function() {
-    document.getElementById('filters-container').innerHTML =
-          '<div class="form-group">'
+    document.getElementById('filters-container').innerHTML +=
+          '<div class="row row-cols-md-auto g-5 align-items-center">'
+          + '<div class="col">'
           + '    <input value="-" type="button" class="form-control del-feed-filter-row" />'
-          + '    <select name="type" class="form-control">'
+          + '</div>'
+          + '<div class="col">'
+          + '    <select name="type" class="form-select">'
           + '        <option value="simple match" selected>simple match</option>'
           + '        <option value="regex">regex</option>'
           + '    </select>'
-          + '    <input type="text" class="form-control"  name="pattern" />'
-          + '    <select name="action_on" class="form-control">'
+          + '</div>'
+          + '<div class="col">'
+          + '    <input type="text" class="form-control" name="pattern" />'
+          + '</div>'
+          + '<div class="col">'
+          + '    <select name="action_on" class="form-select">'
           + '        <option value="match" selected>match</option>'
           + '        <option value="no match">no match</option>'
           + '    </select>'
-          + '    <select name="action" class="form-control">'
+          + '</div>'
+          + '<div class="col">'
+          + '    <select name="action" class="form-select">'
           + '        <option value="mark as read" selected>mark as read</option>'
           + '        <option value="mark as favorite">mark as favorite</option>'
           + '    </select>'
-          + '</div>';
+          + '</div>'
+          + '</div><br />';
   }
 }
 
 var nodes = document.getElementsByClassName('del-feed-filter-row');
 Array.prototype.map.call(nodes, function(node) {
     node.onclick = function() {
-      node.parentNode.remove();
+      node.parentNode.parentNode.remove();
     }
 })
