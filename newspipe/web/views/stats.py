de6f67f3cd656ef4224ec4acc9e3c9255ecbc595
Cédric Bonhomme <cedric@cedricbonhomme.org>
2023-11-09 11:01:09 +0100

    chg: [templates] Added new chart in order to display the repartition of categories.

--- newspipe/web/views/stats.py
+++ newspipe/web/views/stats.py
@@ -4,6 +4,7 @@ from flask_login import current_user
 from flask_login import login_required
 
 from newspipe.controllers import ArticleController
+from newspipe.controllers import CategoryController
 
 stats_bp = Blueprint("stats", __name__, url_prefix="/stats")
 
@@ -15,3 +16,18 @@ stats_bp = Blueprint("stats", __name__, url_prefix="/stats")
 def history(year=None, month=None):
     cntr, _ = ArticleController(current_user.id).get_history(year, month)
     return jsonify(cntr)
+
+
+@stats_bp.route("/categories.json", methods=["GET"])
+@login_required
+def categories(year=None, month=None):
+    """
+    Returns a JSON structure with the list of categories.
+    """
+    categories = CategoryController(current_user.id).read()
+    return jsonify(
+        [
+            {"id": cat.id, "name": cat.name, "count": len(cat.feeds)}
+            for cat in categories
+        ]
+    )
